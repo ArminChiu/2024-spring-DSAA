094aa352d0c98d16a222a642508b05f4989c8807
ArminChiu <Armin_Chiu@outlook.com>
2024-03-27 14:47:28 +0800

    2024/03/27 13:47 v1.0

--- lab1/1.2.c
+++ lab1/1.2.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //给定链表,将节点值大于n的节点删除
 #include <stdio.h>
 #include <stdlib.h>
@@ -64,71 +63,4 @@ int main() {
     }
     puts("");
     return 0;
-=======
-//给定链表,将节点值大于n的节点删除
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <ctype.h>
-#include <assert.h>
-#include <string.h>
-#include <time.h>
-
-
-typedef struct LinkedList {
-    int val;
-    struct LinkedList *next;
-} LinkedList;
-
-void solve(LinkedList *header, int mx) {
-    LinkedList *LNode;
-    LinkedList *LBehind;
-    LNode=header->next;
-    LBehind=header;
-    while(LNode!=NULL){
-        if(LNode->val>mx){
-            if(LNode->next==NULL){
-                LNode->val=666;//啊？神之一手！实现最终debug的居然是这条(说明代码运行逻辑有漏洞)
-                LBehind->next=NULL;
-                return;
-            }else{
-                LNode->val=(LNode->next)->val;
-                LNode->next=(LNode->next)->next;
-            }
-        }else{
-            LBehind=LNode;
-            LNode=LNode->next;
-        }
-    }
-}
-
-int main() {
-    int n, mx;   //n为链表节点数，mx待删除链表阈值
-    scanf("%d%d", &n, &mx);
-    LinkedList *header = NULL;
-    LinkedList *tail = NULL;
-    for (int i = 1; i <= n+1; i++) {
-        int val; scanf("%d", &val);
-        if (!header) {
-            header = (LinkedList *)malloc(sizeof(LinkedList));
-            tail = header;
-            tail->next = NULL;
-            tail->val = val;
-        } else {
-            LinkedList *node =  (LinkedList *)malloc(sizeof(LinkedList));
-            node->val = val;
-            node->next = NULL;
-            tail->next = node;
-            tail = node;
-        }    
-    }
-
-    solve(header, mx);
-
-    for (LinkedList *it = header; it != NULL; it = it->next) {
-        printf("%d ", it->val);
-    }
-    puts("");
-    return 0;
->>>>>>> 1c51882305e1b3390048f7df44206f080b1b99a4
 }
--- lab1/1.3.c
+++ lab1/1.3.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //给定链表,删除倒数第n个节点,并将其后一个节点的值复制到前一个节点
 #include <stdio.h>
 #include <stdlib.h>
@@ -62,69 +61,4 @@ int main() {
     }
     puts("");
     return 0;
-=======
-//给定链表,删除倒数第n个节点,并将其后一个节点的值复制到前一个节点
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <ctype.h>
-#include <assert.h>
-#include <string.h>
-#include <time.h>
-
-
-typedef struct LinkedList {
-    int val;
-    struct LinkedList *next;
-} LinkedList;
-
-
-void solve(LinkedList *header, int remove) {
-    LinkedList *fast=header;
-    LinkedList *low=header;
-    LinkedList *evenlow=low;
-    //fast指针先走remove步
-    for(int i=1;i<=remove;i++)
-        fast=fast->next;
-    while(fast!=NULL){
-        fast=fast->next;
-        evenlow=low;
-        low=low->next;
-    }
-    //退出循环说明low已走至倒数第remove个节点
-    low->val=low->next->val;
-    evenlow->val=low->next->val;
-    low->next=low->next->next;
-}
-
-
-int main() {
-    int n, mx;
-    scanf("%d%d", &n, &mx);  //n为链表节点数，mx即为待删除第mx个节点
-    LinkedList *header = NULL;
-    LinkedList *tail = NULL;
-    for (int i = 1; i <= n+1; i++) {
-        int val; scanf("%d", &val);
-        if (!header) {
-            header = (LinkedList *)malloc(sizeof(LinkedList));
-            tail = header;
-            tail->next = NULL;
-            tail->val = val;
-        } else {
-            LinkedList *node =  (LinkedList *)malloc(sizeof(LinkedList));
-            node->val = val;
-            node->next = NULL;
-            tail->next = node;
-            tail = node;
-        }    
-    }
-
-    solve(header, mx);
-
-    for (LinkedList *it = header; it != NULL; it = it->next) {
-        printf("%d ", it->val);
-    }
-    puts("");
-    return 0;
->>>>>>> 1c51882305e1b3390048f7df44206f080b1b99a4
 }
